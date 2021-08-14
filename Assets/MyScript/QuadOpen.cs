55dfcf810cf55bea4a7f34b1497a527f16473d2b
as <>
2021-08-15 08:32:42 +0900

    Quadを開く

---
+++ Assets/MyScript/QuadOpen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadOpen : MonoBehaviour
+{
+    GameObject quad;
+    Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        quad = GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            renderer.material.SetFloat("_Flip", this.GetComponent<Renderer>().material.GetFloat("_Flip") + -0.01f);
+        }
+    }
+}
