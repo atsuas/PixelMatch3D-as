1cac332aaef47feae6d9474abfe8f9457ae2f8ea
as <>
2021-08-19 04:36:56 +0900

    stageFile2を取ってくる

--- Assets/MyScript/CheckButton.cs
+++ Assets/MyScript/CheckButton.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CheckButton : MonoBehaviour
 {
     Animator animator;
-    public GameObject button;
+    public TextAsset stageFile2;
+    BlockType[,] blockTable;
+    BlocksController[,] blockTableobj;
 
     void Start()
     {
@@ -17,8 +19,47 @@ public class CheckButton : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Click");
+
+            LoadStageFromText2();
+            DebugTable();
         }
     }
 
-    
+    void LoadStageFromText2()
+    {
+        string[] lines = stageFile2.text.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
+        int columns = 10;
+        int rows = 5;
+
+        blockTable = new BlockType[rows, columns];
+        blockTableobj = new BlocksController[rows, columns];
+        for (int y = 0; y < columns; y++)
+        {
+            string[] values = lines[y].Split(new[] { ',' });
+            for (int x = 0; x < rows; x++)
+            {
+                if (values[x] == "0")
+                {
+                    blockTable[x, y] = BlockType.DEATH;
+                }
+                if (values[x] == "1")
+                {
+                    blockTable[x, y] = BlockType.ALIVE;
+                }
+            }
+        }
+    }
+
+    void DebugTable()
+    {
+        for (int y = 0; y < 10; y++)
+        {
+            string debugText = "";
+            for (int x = 0; x < 5; x++)
+            {
+                debugText += blockTable[x, y] + ",";
+            }
+            Debug.Log(debugText);
+        }
+    }
 }
