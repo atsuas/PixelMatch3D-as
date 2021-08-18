7e36a51d822e2ddc4db3ec0e179ebd76d61fc016
as <>
2021-08-18 22:20:47 +0900

    OKボタンが押されたら"Click"表示

--- Assets/MyScript/CheckButton.cs
+++ Assets/MyScript/CheckButton.cs
@@ -4,45 +4,21 @@ using UnityEngine;
 
 public class CheckButton : MonoBehaviour
 {
-    public TextAsset stageFile2;
-    BlockType[,] blockTable;
-    BlocksController[,] blockTableobj;
+    Animator animator;
+    public GameObject button;
 
-    StageManager stageManager;
-    
     void Start()
     {
-        LoadStageFromText2();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
-    }
-
-    void LoadStageFromText2()
-    {
-        string[] lines = stageFile2.text.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
-        int columns = 10;
-        int rows = 5;
-
-        blockTable = new BlockType[rows, columns];
-        blockTableobj = new BlocksController[rows, columns];
-        for (int y = 0; y < columns; y++)
+        if (Input.GetMouseButtonDown(0))
         {
-            string[] values = lines[y].Split(new[] { ',' });
-            for (int x = 0; x < rows; x++)
-            {
-                if (values[x] == "0")
-                {
-                    blockTable[x, y] = BlockType.DEATH;
-                }
-                if (values[x] == "1")
-                {
-                    blockTable[x, y] = BlockType.ALIVE;
-                }
-            }
+            Debug.Log("Click");
         }
     }
 
+    
 }
