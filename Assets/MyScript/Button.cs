ba1f93e9d7ba8d9c2ba28451e35f5cea7a8db672
as <>
2021-08-19 08:39:13 +0900

    動作確認

--- Assets/MyScript/Button.cs
+++ Assets/MyScript/Button.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    public TextAsset stageFile1;
-    public TextAsset stageFile2;
-    BlockType[,] blockType;
-    BlocksController[,] blockTableobj;
-    List<int[]> StageDatas = new List<int[]>() { };
+    public string[] textMessage;
+    public string[,] textWords;
 
-    public BlocksController blockPrefab;
+    private int rowLength;
+    private int columnLength;
 
     void Start()
     {
-        
-    }
+        TextAsset textAsset = new TextAsset();
+        textAsset = Resources.Load("Test", typeof(TextAsset)) as TextAsset;
+        string TextLines = textAsset.text;
 
-    void Update()
-    {
-        
+        textMessage = TextLines.Split('\n');
+
+        columnLength = textMessage[0].Split('\t').Length;
+        rowLength = textMessage.Length;
+
+        textWords = new string[rowLength, columnLength];
+
+        for (int i = 0; i < rowLength; i++)
+        {
+            string[] tempWords = textMessage[i].Split('\t');
+
+            for (int n = 0; n < columnLength; n++)
+            {
+                textWords[i, n] = tempWords[n];
+                Debug.Log(textWords[i, n]);
+            }
+        }
     }
 }
