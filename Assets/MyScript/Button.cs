b453ce34ab5686b5d95e59b82b0e6c2db1941763
as <>
2021-08-19 08:03:22 +0900

    動作確認用

---
+++ Assets/MyScript/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public TextAsset stageFile1;
+    public TextAsset stageFile2;
+    BlockType[,] blockType;
+    BlocksController[,] blockTableobj;
+    List<int[]> StageDatas = new List<int[]>() { };
+
+    public BlocksController blockPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
